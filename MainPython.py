940dfe0594ef3166ff27a96f77650de773712c2a
OscarZam02062004 <zamoraoscar964@gmail.com>
2023-07-15 14:37:18 -0600

    Eres mayor de edad o no

--- MainPython.py
+++ MainPython.py
@@ -1,3 +1,9 @@
 print("Hola mundo")
-nombre=input("Como te llamas")
-edad=int(input("Ingresa tu edad"))
+print("-----------")
+nombre=input("Como te llamas: ")
+edad=int(input("Ingresa tu edad: "))
+print("------------")
+if edad>=18:
+    print("Eres mayor de edad ")
+else:
+    print("No eres mayor de edad")
